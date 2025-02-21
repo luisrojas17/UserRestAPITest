9061d11f8c012151401783dd1d2f2e6a360147e8
Jose Luis Rojas Gomez <amarillo17cyl@gmail.com>
2025-02-20 22:24:54 -0600

    It was added new version of getUsers method. The external API is concuming through HttpClient, HttpRequest, HttpResponse classes provided by JDK.

--- src/main/java/com/example/api/controller/UserController.java
+++ src/main/java/com/example/api/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.api.controller;
 import com.example.api.controller.model.User;
 import com.example.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
 
         return userService.getUsers();
 
     }
+
+    @GetMapping(value = "/users2", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getUsers2() {
+        return ResponseEntity.ok().body(userService.getUsers2());
+     }
 
 }
--- src/main/java/com/example/api/service/UserService.java
+++ src/main/java/com/example/api/service/UserService.java
@@ -9,6 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,9 +36,45 @@ public class UserService {
                 null,
                 new ParameterizedTypeReference<>(){});
 
-        log.info("Users gotten: [{}]", response.getBody());
+        log.info("Status code: [{}]", response.getStatusCode());
+        log.info("Users gotten: [{}]", (Object) response.getBody());
 
         return Arrays.asList(response.getBody());
+
+    }
+
+    /**
+     * This method uses HttpClient to consume the API users.
+     *
+     * @return an instance of string like json format.
+     */
+    public String getUsers2() {
+
+        String strResponse = "";
+
+        try {
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(URL))
+                    .timeout(Duration.ofSeconds(10))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            log.info("Status code: [{}]", response.statusCode());
+            log.info("Users gotten through HttpClient: [{}]", response.body());
+
+            strResponse = response.body();
+
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            log.error("Error: [{}]", e.getMessage());
+        }
+
+        return strResponse;
 
     }
 
